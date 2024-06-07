17ec1899a79312d2867634fb4e583933e66c04d0
kirangajjana <kiran.gajjana@gmail.com>
2024-06-07 15:23:12 +0530

    updated templates

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
 app=Flask(__name__)
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return 
 
+
+@app.route('/login')
+def login():
+    return
+
+
+@app.route('/signup')
+def signup():
+    return 
 
 if __name__=="__main__":
     app.run(debug=True)
