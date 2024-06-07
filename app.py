b73aee02a4831b835ee1d39d3b838690779098b1
kirangajjana <kiran.gajjana@gmail.com>
2024-06-07 15:34:35 +0530

    updated templated

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 app=Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
 def home():
-    return 
+    return render_template('home.html',title='home')
 
 
 @app.route('/login')
 def login():
-    return
+    return render_template('login.html',title='login')
 
 
 @app.route('/signup')
 def signup():
-    return 
+    return render_template('signup.html',title='signup')
 
 if __name__=="__main__":
     app.run(debug=True)
